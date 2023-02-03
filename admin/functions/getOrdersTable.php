d6dbdead1be4eca91c19ac53496b26fe596db704
jeanc <jeancamlap@gmail.com>
2023-02-03 15:34:56 +0100

    test multiple receipts

--- admin/functions/getOrdersTable.php
+++ admin/functions/getOrdersTable.php
@@ -1,6 +1,36 @@
 <?php
 session_start();
 
+function getZipFromReceipt($receipt)
+{
+    // Set the zip file name
+    $zipFileName = "receipts.zip";
+
+    // Get all the files to be added to the zip archive
+    $files = glob("receipts/" . $receipt ."_*.png");
+
+    // Create a new ZipArchive object
+    $zip = new ZipArchive();
+
+    // Open the zip archive for writing
+    if ($zip->open($zipFileName, ZipArchive::CREATE) === TRUE) {
+
+        // Add each file to the zip archive
+        foreach ($files as $file) {
+            $zip->addFile($file, basename($file));
+        }
+
+        // Close the zip archive
+        $zip->close();
+
+        // Provide a download link for the zip archive
+        return "<a id='download' href='$zipFileName' download='$zipFileName'>Download Receipts</a>";
+
+    } else {
+        // Error message if the zip archive could not be created
+        return "Error: Zip archive could not be created.";
+    }
+}
 if(isset($_SESSION["admin"]) && !empty($_SESSION["admin"])) {
     $adminUsername = $_SESSION["admin"];
 
@@ -47,7 +77,7 @@ $data = array();
 if(isset($orderDetails) && !empty($orderDetails)){
     foreach ($orderDetails as $key => $orderDetail) {
         if($orderDetail["status"] == "Completed"){
-            $action = "<a id='download' href='receipts/".$orderDetail["id"].".png' download='".$orderDetail["id"].".png'>Download Receipt</a>";
+            $action = getZipFromReceipt($orderDetail["id"]);
         }elseif($orderDetail["status"] == "Failed"){
             $action = "<a href='?id=".$orderDetail["id"]."&status=Failed'>Delete Order</a>";
         }else{
